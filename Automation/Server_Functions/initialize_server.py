be6297dcdcbefc7aa75ae789f8209c5ff47d1fcb
Vinícius <viniciushallmann2015@gmail.com>
2025-02-25 12:13:40 -0300

    remove ip return

--- Automation/Server_Functions/initialize_server.py
+++ Automation/Server_Functions/initialize_server.py
@@ -10,7 +10,12 @@ def initialize_server():
         AWS.start_instance(ec2, config["INSTANCE_ID"])
         ip = AWS.get_instance_ip(ec2, config["INSTANCE_ID"])
         print("Instância iniciada.")
+        """NAO FUNCIONA O RETORNO DO IP
+            - Tem que esperar o estado "executando" para resgatar IP
+        
         print(f"IP da instância: {ip}")
+        
+        """
         return ec2, ip
     except Exception as e:
         print(f"Erro ao inicializar instância: {e}")
