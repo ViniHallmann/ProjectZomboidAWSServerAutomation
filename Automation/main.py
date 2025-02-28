28a911e7f662f6f23817643d3d0137598a10cb07
VineMaionese <viniciushallmann2015@gmail.com>
2025-02-28 12:47:11 -0300

    added new commands parameters

--- Automation/main.py
+++ Automation/main.py
@@ -5,14 +5,30 @@ from typing import Dict, Callable
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Automation.Server_Functions.initialize_server import initialize_instance
+from Automation.Server_Functions.initialize_server import start_server
 from Automation.Server_Functions.stop_server import stop_server_and_instance
+from Automation.Server_Functions.stop_server import stop_server
+from Automation.Server_Functions.stop_server import stop_instance
 from Automation.Server_Functions.update_ini import update_ini
 
-USAGE_MESSAGE = "Uso: python main.py <start>||<stop>"
+USAGE_MESSAGE = """
+Uso: python main.py <comando> [opções]
+
+Comandos disponíveis:
+  start-instance   - Inicia a instância EC2 (o servidor inicia automaticamente)
+  start-server     - Inicia o servidor do Zomboid
+  terminate        - Para o servidor e a instância EC2
+  stop-server      - Para apenas o servidor do Zomboid
+  stop-instance    - Para apenas a instância EC2
+  update [caminho] - Atualiza o arquivo Server.ini (caminho opcional)
+"""
 UNKNOWN_COMMAND_MESSAGE = "Comando desconhecido: {}"
 COMMANDS: Dict[str, Callable] = {
-    "start": initialize_instance,
-    "stop": stop_server_and_instance,
+    "start-instance": initialize_instance, #INICIAR A INSTANCIA INICIA O SERVER JUNTO
+    "start-server": start_server,
+    "terminate": stop_server_and_instance,
+    "stop-server": stop_server,
+    "stop-instance": stop_instance,
     "update": update_ini
 }
 
